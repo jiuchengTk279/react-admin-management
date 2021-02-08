c71bb9c8a28f240ea1a41ca75e933c11d88eb7a8
jiucheng <15279232381@163.com>
2021-02-08 14:51:40 +0800

    完成登录组件的功能

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,10 +1,134 @@
 import React, { Component } from 'react'
+import { Form, Icon, Input, Button, message } from 'antd'
+import './login.less'
+import logo from '../../assets/images/logo.png'
+
+const Item = Form.Item
 
 // 登录的路由组件
-export default class Login extends Component {
+class Login extends Component {
+
+  // 点击提交表单事件
+  handleSubmit = (event) => {
+    // 阻止事件的默认行为
+    event.preventDefault()
+
+    // 对所有表单字段进行检验
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log(values)
+      } else {
+        console.log(err)
+      }
+    })
+
+    // const form = this.props.form
+    // // 获取表单项的输入数据
+    // const values = form.getFieldsValue()
+    // console.log(values)
+  }
+
+  // 对密码进行自定义验证
+  validPwd = (rule, value, callback) => {
+    if (!value) {
+      callback('密码必须输入')
+    } else if (value.length < 4) {
+      callback('密码长度不能小于4位')
+    } else if (value.length > 12) {
+      callback('密码长度不能大于12位')
+    } else if (!/^[a-zA-Z0-9_]+$/.test(value)) {
+      callback('密码必须是英文、数字或下划线组成')
+    } else {
+      callback()
+    }
+  }
+
+
   render () {
+    // 得到具强大功能的form对象
+    const form = this.props.form
+    const { getFieldDecorator } = form
+
     return (
-      <div>Login</div>
+      <div className="login">
+        <header className="login-header">
+          <img src={logo} alt="logo"></img>
+          <h1>React 后台管理系统</h1>
+        </header>
+        <section className="login-content">
+          <h2>用户登录</h2>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Item>
+              {
+                getFieldDecorator('username', { // 配置对象: 属性名是特定的一些名称
+                  // 声明式验证: 直接使用别人定义好的验证规则进行验证
+                  rules: [
+                    { required: true, whitespace: true, message: '用户名必须输入'},
+                    { min: 4, message: '用户名至少是4位'},
+                    { max: 12, message: '用户名至少是12位'},
+                    { pattern: /^[a-zA-Z0-9_]+$/, message: '用户名必须是英文、数字或下划线组成'}
+                  ],
+                  initialValue: 'admin', // 初始值
+                })(
+                  <Input
+                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                    placeholder="用户名"
+                  />
+                )
+              }
+            </Form.Item>
+            <Form.Item>
+              {
+                getFieldDecorator('password', {
+                  rules: [
+                    { validator: this.validPwd }
+                  ]
+                })(
+                  <Input
+                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                    type="password"
+                    placeholder="密码"
+                  />
+                )
+              }
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                  登陆
+              </Button>
+            </Form.Item>
+          </Form>
+        </section>
+      </div>
     )
   }
 }
+
+
+/*
+1. 高阶函数
+    1). 一类特别的函数
+        a. 接受函数类型的参数
+        b. 返回值是函数
+    2). 常见
+        a. 定时器: setTimeout()/setInterval()
+        b. Promise: Promise(() => {}) then(value => {}, reason => {})
+        c. 数组遍历相关的方法: forEach()/filter()/map()/reduce()/find()/findIndex()
+        d. 函数对象的bind()
+        e. Form.create()() / getFieldDecorator()()
+    3). 高阶函数更新动态, 更加具有扩展性
+
+2. 高阶组件
+    1). 本质就是一个函数
+    2). 接收一个组件(被包装组件), 返回一个新的组件(包装组件), 包装组件会向被包装组件传入特定属性
+    3). 作用: 扩展组件的功能
+    4). 高阶组件也是高阶函数: 接收一个组件函数, 返回是一个新的组件函数
+ */
+
+
+/**
+ * 包装Form组件生成一个新的组件: Form(Login)
+  新组件会向Form组件传递一个强大的对象属性: form
+ */
+const WrapLogin = Form.create()(Login)
+export default WrapLogin
