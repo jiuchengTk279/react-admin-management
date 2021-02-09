3bd468ccb9c814e0d5ba19de4470257279e9b7c3
jiucheng <15279232381@163.com>
2021-02-09 20:21:58 +0800

    完成左侧菜单导航的功能

--- src/components/leftNav/index.jsx
+++ src/components/leftNav/index.jsx
@@ -1,12 +1,115 @@
 import React, { Component } from 'react'
+import { Link, withRouter } from 'react-router-dom'
+import { Menu, Icon } from 'antd'
+import logo from '../../assets/images/logo.png'
+import menuList from '../../config/menuConfig'
 import './index.less'
 
-export default class LeftNav extends Component {
+const { SubMenu } = Menu
+class LeftNav extends Component {
+
+  // 根据menu的数据数组生成对应的标签数组
+  // 使用map() + 递归调用
+  getMenuNodes_map = (menuList) => {
+    return menuList.map(item => {
+      if (!item.children) {
+        return (
+          <Menu.Item key={item.key}>
+            <Link to={item.key}>
+              <Icon type={item.icon}></Icon>
+              <span>{item.title}</span>
+            </Link>
+          </Menu.Item>
+        )
+      } else {
+        return (
+          <SubMenu key={item.key} title={
+            <span>
+              <Icon type={item.icon}></Icon>
+              <span>{item.title}</span>
+            </span>
+          }>
+            { this.getMenuNodes(item.children) }
+          </SubMenu>
+        )
+      }
+    })
+  }
+
+  // 根据menu的数据数组生成对应的标签数组
+  // 使用reduce() + 递归调用
+  getMenuNodes = (menuList) => {
+
+    const path = this.props.location.pathname
+
+    return menuList.reduce((pre, item) => {
+      if (!item.children) {
+        pre.push((
+          <Menu.Item key={item.key}>
+            <Link to={item.key}>
+              <Icon type={item.icon}></Icon>
+              <span>{item.title}</span>
+            </Link>
+          </Menu.Item>
+        ))
+      } else {
+        // 查找一个与当前请求路径匹配的子Item
+        const cItem = item.children.find(cItem => cItem.key === path)
+        // 如果存在, 说明当前item的子列表需要打开
+        if (cItem) {
+          this.openKey = item.key
+        }
+
+        pre.push((
+          <SubMenu key={item.key} title={
+            <span>
+              <Icon type={item.icon}></Icon>
+              <span>{item.title}</span>
+            </span>
+          }>
+            { this.getMenuNodes(item.children) }
+          </SubMenu>
+        ))
+      }
+      return pre
+    }, [])
+  }
+
+  // 在第一次render()之前执行一次
+  // 为第一个render()准备数据(必须同步的)
+  componentWillMount () {
+    this.menuNodes = this.getMenuNodes(menuList)
+  }
 
   render () {
+    // 得到当前请求的路由路径
+    const path = this.props.location.pathname
+    // 得到需要打开菜单项的key
+    const openKey = this.openKey
+  
     return (
-      <div className="left-nav">LeftNav</div>
+      <div className="left-nav">
+        <Link to="/" className="left-nav-header">
+          <img src={logo} alt="logo"></img>
+          <h1>React 后台</h1>
+        </Link>
+
+        <Menu
+          mode="inline"
+          theme="dark"
+          selectedKeys={[path]}
+          defaultOpenKeys={[openKey]}
+        >
+          { this.menuNodes }
+        </Menu>
+      </div>
     )
   }
 }
 
+/*
+withRouter高阶组件:
+包装非路由组件, 返回一个新的组件
+新的组件向非路由组件传递3个属性: history/location/match
+ */
+export default withRouter(LeftNav)
--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
-import { Redirect } from 'react-router-dom'
+import { Redirect, Route, Switch } from 'react-router-dom'
 import { Layout } from 'antd'
 import memoryUtils from '../../utils/memoryUtils'
 import MHeader from '../../components/header'
 import LeftNav from '../../components/leftNav'
+import Home from '../home/home'
+import Category from '../category/category'
+import Product from '../product/product'
+import Role from '../role/role'
+import User from '../user/user'
+import Bar from '../charts/bar'
+import Line from '../charts/line'
+import Pie from '../charts/pie'
 
-const { Header, Footer, Sider, Content } = Layout
+const { Footer, Sider, Content } = Layout
 
 // 后台管理的路由组件
 export default class Admin extends Component {
@@ -25,7 +33,19 @@ export default class Admin extends Component {
         </Sider>
         <Layout>
           <MHeader>Header</MHeader>
-          <Content style={{margin: 20, backgroundColor: '#fff'}}>Content</Content>
+          <Content style={{margin: 20, backgroundColor: '#fff'}}>
+            <Switch>
+              <Route path="/home" component={Home}></Route>
+              <Route path="/category" component={Category}></Route>
+              <Route path="/product" component={Product}></Route>
+              <Route path="/role" component={Role}></Route>
+              <Route path="/user" component={User}></Route>
+              <Route path="/charts/bar" component={Bar}></Route>
+              <Route path="/charts/line" component={Line}></Route>
+              <Route path="/charts/pie" component={Pie}></Route>
+              <Redirect to="/home"></Redirect>
+            </Switch>
+          </Content>
           <Footer style={{textAlign: 'center', color: '#cccccc'}}>推荐使用谷歌浏览器，可以获得更佳页面操作体验</Footer>
         </Layout>
       </Layout>
