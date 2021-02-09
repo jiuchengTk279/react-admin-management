32d2e2e526aa7dddf5599faa9d4b210dbcd37b59
jiucheng <15279232381@163.com>
2021-02-09 22:25:11 +0800

    完成头部组件的功能

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,11 +1,106 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+import { Modal, message } from 'antd'
+import { formateDate } from '../../utils/dateUtils'
+import memoryUtils from '../../utils/memoryUtils'
+import storageUtils from '../../utils/storageUtils'
+import menuList from '../../config/menuConfig'
+import LinkButton from '../linkButton'
+import { reqWeather } from '../../api'
 import './index.less'
 
-export default class Header extends Component {
+const { confirm } = Modal
+class Header extends Component {
+
+  state = {
+    currentTime: formateDate(Date.now()), // 当前时间字符串
+    dayPictureUrl: '', // 天气图片url
+    weather: '' // 天气的文本
+  }
+
+  getTime = () => {
+    this.intervalId = setInterval(() => {
+      const currentTime = formateDate(Date.now())
+      this.setState({ currentTime })
+    }, 1000)
+  }
+
+  getWeather = async () => {
+    const { dayPictureUrl, weather } = await reqWeather('北京')
+    this.setState({ dayPictureUrl, weather })
+  }
+
+  getTitle = () => {
+    const path = this.props.location.pathname
+    let title
+    menuList.forEach(item => {
+      if (item.key === path) {
+        title = item.title
+      } else if (item.children) {
+        const cItem = item.children.find(cItem => cItem.key === path)
+        if (cItem) {
+          title = cItem.title
+        }
+      }
+    })
+    return title
+  }
+
+  logout = () => {
+    confirm({
+      // title: '警告提示',
+      content: '确定退出吗？',
+      onOk: () => {
+        // 删除保存的 user 数据
+        storageUtils.removeUser()
+        memoryUtils.user = {}
+        // 跳转到 login
+        this.props.history.replace('/login')
+        message.success('退出成功')
+      },
+      onCancel() {
+        message.info('已取消退出')
+      },
+    });
+  }
+
+  // 第一次render()之后执行一次
+  // 般在此执行异步操作: 发ajax请求/启动定时器
+  componentDidMount () {
+    // 获取当前的时间
+    this.getTime()
+    // 获取当前天气
+    this.getWeather()
+  }
+
+  // 当前组件卸载之前调用
+  componentWillUnmount () {
+    // 清除定时器
+    clearInterval(this.intervalId)
+  }
 
   render () {
+    const { currentTime, dayPictureUrl, weather } = this.state
+    const username = memoryUtils.user.username
+    const title = this.getTitle()
+
     return (
-      <div className="header">Header</div>
+      <div className="header">
+        <div className="header-top">
+          <span>欢迎，{ username }</span>
+          <LinkButton href="javascript:" onClick={this.logout}>退出</LinkButton>
+        </div>
+        <div className="header-bottom">
+          <div className="header-bottom-left">{ title }</div>
+          <div className="header-bottom-right">
+            <span>{ currentTime }</span>
+            <img src={ dayPictureUrl } alt="weather"></img>
+            <span>{ weather }</span>
+          </div>
+        </div>
+      </div>
     )
   }
 }
+
+export default withRouter(Header)
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-
+import './home.less'
 
 export default class Home extends Component {
   
   render () {
     return (
-      <div>
-        home
+      <div className="home">
+        欢迎使用 React 后台管理系统
       </div>
     )
   }
