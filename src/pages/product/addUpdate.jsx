8359341123cbdf877076afd9ca33704a3e50e00a
jiucheng <15279232381@163.com>
2021-02-18 16:35:08 +0800

    完成添加商品的表单验证

--- src/pages/product/addUpdate.jsx
+++ src/pages/product/addUpdate.jsx
@@ -5,9 +5,27 @@ import LinkButton from '../../components/linkButton'
 const {Item} = Form
 const { TextArea } = Input
 
-export default class ProductAddUpdate extends Component {
+class ProductAddUpdate extends Component {
+
+  // 提交表单
+  submit = () => {
+    this.props.form.validateFields((error, values) => {
+      if (!error) {
+        console.log('发送ajax请求')
+      }
+    })
+  }
+
+  // 验证价格的自定义验证函数
+  validatePrice = (rule, value, callback) => {
+    if (value*1 > 0) {
+      callback() // 验证通过
+    } else {
+      callback('价格必须大于0') // 验证没通过
+    }
+  }
+
   render () {
-    
     // 指定Item布局的配置对象
     const formItemLayout = {
       labelCol: { span: 2}, // 左侧label的宽度
@@ -23,20 +41,54 @@ export default class ProductAddUpdate extends Component {
       </span>
     )
 
+    const { getFieldDecorator } = this.props.form
+
     return (
       <Card title={title}>
         <Form {...formItemLayout}>
           <Item label="商品名称">
-            <Input placeholder="请输入商品名称"></Input>
+            {
+              getFieldDecorator('name', {
+                initialValue: '',
+                rules: [{ required: true, message: '必须输入商品名称'}]
+              })(<Input placeholder="请输入商品名称"></Input>)
+            }
           </Item>
           <Item label="商品描述">
-            <TextArea placeholder="请输入商品描述" autosize={{ minRows: 2, maxRows: 6}}></TextArea>
+            {
+              getFieldDecorator('desc', {
+                initialValue: '',
+                rules: [{ required: true, message: '必须输入商品描述'}]
+              })(<TextArea placeholder="请输入商品描述" autosize={{ minRows: 2, maxRows: 6}}></TextArea>)
+            }
           </Item>
           <Item label="商品价格">
-            <Input type="number" placeholder="请输入商品价格" addonAfter="元"></Input>
+            {
+              getFieldDecorator('price', {
+                initialValue: '',
+                rules: [
+                  { required: true, message: '必须输入商品价格'},
+                  { validator: this.validatePrice }
+                ]
+              })(<Input type="number" placeholder="请输入商品价格" addonAfter="元"></Input>)
+            }
+          </Item>
+          <Item label="商品分类">
+            <div>商品分类</div>
+          </Item>
+          <Item label="商品图片">
+            <div>商品图片</div>
+          </Item>
+          <Item label="商品详情">
+            <div>商品详情</div>
+          </Item>
+          <Item>
+            <Button type="primary" onClick={this.submit}>提交</Button>
           </Item>
         </Form>
       </Card>
     )
   }
 } 
+
+export default Form.create()(ProductAddUpdate)
